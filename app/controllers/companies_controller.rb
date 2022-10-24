08c7304f04cfef28ee80614e15834f4f7697ba25
Danniel Albuquerque <danniel.master@gmail.com>
2022-10-24 16:55:42 -0300

    cadasto e edição de empresa e ajustes

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,13 +1,29 @@
 class CompaniesController < ApplicationController
   def new
+    @company = current_user.build_company
   end
 
   def create
+    @company = current_user.build_company(company_params)
+    if @company.save
+      flash[:notice] = 'Empresa cadastrada com sucesso.'
+      redirect_to root_path
+    else
+      flash[:alert] = 'Erro ao cadastrar empresa'
+      render :new
+    end
   end
 
   def edit
+    @company = current_user.company
   end
 
   def update
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :url, :logo)
   end
 end
