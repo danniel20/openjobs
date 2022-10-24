8ae1fd154b3433c0aee852217ace11db0112c0e9
Danniel Albuquerque <danniel.master@gmail.com>
2022-10-24 12:03:33 -0300

    criação de controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :companies, only: [:new, :edit, :create, :upadte]
+  resources :positions
+  resources :applicants, only: [:index, :new, :create]
+
   devise_for :users
   root 'home#index'
 end
