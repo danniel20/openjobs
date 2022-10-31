ae69468381db80c8f01d627646d0b0cfd34c09c6
Danniel Albuquerque <danniel.master@gmail.com>
2022-10-31 16:56:57 -0300

    ajustes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  subject { User.new(email: "user@example.email", password: "123456", password_confirmation: "123456") }
-
   context 'validations' do
     it { is_expected.to be_valid }
     it { is_expected.to validate_presence_of(:email) }
